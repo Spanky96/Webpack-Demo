c1a430f49d51ac212e4da2f646679e8c86acbfdf
Spanky <819062398@qq.com>
2019-05-21 23:59:51 +0800

    5 21

--- src/language.js
+++ src/language.js
@@ -21,6 +21,36 @@ module.exports = [{
   section1_m_2p: 'Get converted coin <br>when you run sport data, <br>activity reward as well',
   section1_m_3t: 'Get Reward',
   section1_m_3p: 'Log in and<br>pick up your award',
+
+  section3_t_1: 'Basic',
+  section3_t_2: 'Medium',
+  section3_t_3: 'Premium',
+  section3_t_4: 'Supreme',
+
+  section3_basic_1: 'DJI Osmo Action Cam Digital Camera with 2 displays 11m waterproof 4K HDR-Video 12MP 145° Angle Black<br>Osmo Action helps you record the passage of time more easily than ever. Just mark where you want the camera movement to start and end. and tap ‘start’ to create phenomenal moving timelapses without additional specialist equipment.',
+  section3_basic_2: 'AutoCare Formaldehyde Romover. <br>Visible High-efficiency formaldehyde VOC measurement & absorption, serial applicated in OEM Audi、Ford、BMW、GM models, fully body-harmless . Suitable for New vehicle air quality improvement , in-house air testing and VOC removal. AutoCare oriented from Nanjing University Lab , dedicate to help families having safety air, green life.',
+  section3_medium_1: 'DJI Osmo Action Cam Digital Camera with 2 displays 11m waterproof 4K HDR-Video 12MP 145° Angle Black<br>Osmo Action helps you record the passage of time more easily than ever. Just mark where you want the camera movement to start and end. and tap ‘start’ to create phenomenal moving timelapses without additional specialist equipment.',
+  section3_medium_2: 'AutoCare Formaldehyde Romover. <br>Visible High-efficiency formaldehyde VOC measurement & absorption, serial applicated in OEM Audi、Ford、BMW、GM models, fully body-harmless . Suitable for New vehicle air quality improvement , in-house air testing and VOC removal. AutoCare oriented from Nanjing University Lab , dedicate to help families having safety air, green life.',
+  section3_premium_1: 'DJI Osmo Action Cam Digital Camera with 2 displays 11m waterproof 4K HDR-Video 12MP 145° Angle Black<br>Osmo Action helps you record the passage of time more easily than ever. Just mark where you want the camera movement to start and end. and tap ‘start’ to create phenomenal moving timelapses without additional specialist equipment.',
+  section3_premium_2: 'AutoCare Formaldehyde Romover. <br>Visible High-efficiency formaldehyde VOC measurement & absorption, serial applicated in OEM Audi、Ford、BMW、GM models, fully body-harmless . Suitable for New vehicle air quality improvement , in-house air testing and VOC removal. AutoCare oriented from Nanjing University Lab , dedicate to help families having safety air, green life.',
+  section3_superme_1: 'DJI Osmo Action Cam Digital Camera with 2 displays 11m waterproof 4K HDR-Video 12MP 145° Angle Black<br>Osmo Action helps you record the passage of time more easily than ever. Just mark where you want the camera movement to start and end. and tap ‘start’ to create phenomenal moving timelapses without additional specialist equipment.',
+  section3_superme_2: 'AutoCare Formaldehyde Romover. <br>Visible High-efficiency formaldehyde VOC measurement & absorption, serial applicated in OEM Audi、Ford、BMW、GM models, fully body-harmless . Suitable for New vehicle air quality improvement , in-house air testing and VOC removal. AutoCare oriented from Nanjing University Lab , dedicate to help families having safety air, green life.',
+
+  section4_t: 'Balance Distribution & Reward Mechanism ',
+  section4_p_1: 'Persistent',
+  section4_p_2: 'Steps',
+  section4_p_3: 'Invitation',
+  section4_p_4: 'Subscription',
+  section4_p_5: 'Game',
+  section4_p_6: 'Contribution',
+  section4_p_7: 'Popularity',
+
+  section5_step: 'Step',
+  section5_pet_t: 'Crypto Pet have the unique gene, stepbook allow our runner own their own blockchain tech based pets, as well your dog in real world, accompany with you, have fun, pet social, participant in game and rewarding. Furthermore, crypto pet will never die.',
+  section5_pet_1: 'Austrlia Rabbit',
+  section5_pet_2: 'Smile Frog',
+  section5_pet_3: 'Crazy Penguin',
+
 }, {
   // 中文
   // header部分
@@ -45,6 +75,36 @@ module.exports = [{
   section1_m_2p: '坚持更新运动数据<br>并转换成stepbook奖励积分<br>',
   section1_m_3t: '数据变现',
   section1_m_3p: '直接兑换赞助商品<br>或者耐心等待交易所上市套现<br>',
+
+  section3_t_1: '入门级赞助',
+  section3_t_2: '高级赞助',
+  section3_t_3: '卓越级赞助',
+  section3_t_4: '至尊级赞助',
+
+  section3_basic_1: 'DJI 大疆Osmo Action 灵眸运动相机前后双屏防抖防水<br>双屏一体，稳定如一。前置彩屏让自拍构图更精准，从此告别盲拍，自拍更有型，时刻展示好状态。RockSteady增稳抖动算法，滤除抖动同时，还能保持相机的转向运动，带来稳定流畅的拍摄画面。',
+  section3_basic_2: 'AutoCare 甲醛检测/吸收一体式魔方<br>可视化的甲醛VOC检测和吸收材料，适用于汽车甲醛测试和吸收，家庭装修甲醛测试和吸收。简单易用，安全无毒，甲醛VOC吸收后核心材料颜色发生变色现象。AutoCare已经在奥迪、福特、通用、宝马等主机车型完成测试，南京大学实验室技术提供。',
+  section3_medium_1: 'DJI 大疆Osmo Action 灵眸运动相机前后双屏防抖防水<br>双屏一体，稳定如一。前置彩屏让自拍构图更精准，从此告别盲拍，自拍更有型，时刻展示好状态。RockSteady增稳抖动算法，滤除抖动同时，还能保持相机的转向运动，带来稳定流畅的拍摄画面。',
+  section3_medium_2: 'AutoCare 甲醛检测/吸收一体式魔方<br>可视化的甲醛VOC检测和吸收材料，适用于汽车甲醛测试和吸收，家庭装修甲醛测试和吸收。简单易用，安全无毒，甲醛VOC吸收后核心材料颜色发生变色现象。AutoCare已经在奥迪、福特、通用、宝马等主机车型完成测试，南京大学实验室技术提供。',
+  section3_premium_1: 'DJI 大疆Osmo Action <br>双屏一体，稳定如一。前置彩屏让自拍构图更精准，从此告别盲拍，自拍更有型，时刻展示好状态。RockSteady增稳抖动算法，滤除抖动同时，还能保持相机的转向运动，带来稳定流畅的拍摄画面。',
+  section3_premium_2: 'AutoCare 甲醛检测/吸收一体式魔方<br>可视化的甲醛VOC检测和吸收材料，适用于汽车甲醛测试和吸收，家庭装修甲醛测试和吸收。简单易用，安全无毒，甲醛VOC吸收后核心材料颜色发生变色现象。AutoCare已经在奥迪、福特、通用、宝马等主机车型完成测试，南京大学实验室技术提供。',
+  section3_superme_1: 'DJI 大疆Osmo Action 灵眸运动相机前后双屏防抖防水<br>双屏一体，稳定如一。前置彩屏让自拍构图更精准，从此告别盲拍，自拍更有型，时刻展示好状态。RockSteady增稳抖动算法，滤除抖动同时，还能保持相机的转向运动，带来稳定流畅的拍摄画面。',
+  section3_superme_2: 'AutoCare 甲醛检测/吸收一体式魔方<br>可视化的甲醛VOC检测和吸收材料，适用于汽车甲醛测试和吸收，家庭装修甲醛测试和吸收。简单易用，安全无毒，甲醛VOC吸收后核心材料颜色发生变色现象。AutoCare已经在奥迪、福特、通用、宝马等主机车型完成测试，南京大学实验室技术提供。',
+
+  section4_t: '积分代币分配机制&奖励分配机制',
+  section4_p_1: '坚持力',
+  section4_p_2: '步数',
+  section4_p_3: '邀请',
+  section4_p_4: '粉丝',
+  section4_p_5: '游戏',
+  section4_p_6: '贡献度',
+  section4_p_7: '活跃度',
+
+  section5_step: '步',
+  section5_pet_t: 'stepbook提供基于区块链的加密虚拟宠物提供领养，每一个宠物都是独一无二的基因，跟真实的小猫小狗一样可以繁殖、社交、遛养，它可以带来很多虚拟玩法的可能性，并且“好血缘“的加密宠物是稀缺的，它将成为你在运动时候的最好伴侣，有时候，还可以给主人带来真实的奖励。另外，它的一个优点是，永远陪伴你。',
+  section5_pet_1: '澳洲大兔',
+  section5_pet_2: '微笑青蛙',
+  section5_pet_3: '疯狂企鹅',
+
 }];
 
 // in18="section1_top"
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -96,6 +96,18 @@ $(function(){
     }
   });
 
+  $('.elbtns .btn').click(function (e) {
+    if ($(e.currentTarget).hasClass('active')) {
+      return;
+    }
+    $('.elbtns .btn').removeClass('active');
+    $(e.currentTarget).addClass('active')
+    var bondType = $(e.currentTarget).attr('type');
+    $('.section3 .slider-wrapper').hide();
+    $('.' + bondType).show();
+
+  });
+
 
 
   // section5 时间刷新
@@ -127,3 +139,157 @@ $(function(){
   }
 
 });
+
+(function($){
+
+	$.fn.et_simple_slider = function( options ) {
+		var settings = $.extend( {
+			slide         			: '.et-slide',				 	// slide class
+			arrows					: '.et-slider-arrows',			// arrows container class
+			prev_arrow				: '.et-arrow-prev',				// left arrow class
+			next_arrow				: '.et-arrow-next',				// right arrow class
+			controls 				: '.et-controllers a',			// control selector
+			control_active_class	: 'et-active-control',			// active control class name
+			previous_text			: 'Previous',					// previous arrow text
+			next_text				: 'Next',						// next arrow text
+			fade_speed				: 500,							// fade effect speed
+			use_arrows				: true,							// use arrows?
+			use_controls			: true,							// use controls?
+			manual_arrows			: '',							// html code for custom arrows
+			append_controls_to		: '',							// controls are appended to the slider element by default, here you can specify the element it should append to
+			controls_class			: 'et-controllers',				// controls container class name
+			slideshow				: false,						// automattic animation?
+			slideshow_speed			: 7000							// automattic animation speed
+		}, options );
+
+		return this.each( function() {
+			var $et_slider 			= $(this),
+				$et_slide			= $et_slider.find( settings.slide ),
+				et_slides_number	= $et_slide.length,
+				et_fade_speed		= settings.fade_speed,
+				et_active_slide		= 0,
+				$et_slider_arrows,
+				$et_slider_prev,
+				$et_slider_next,
+				$et_slider_controls,
+				et_slider_timer,
+				controls_html = '';
+
+			if ( settings.use_arrows && et_slides_number > 1 ) {
+				if ( settings.manual_arrows == '' )
+					$et_slider.append( '<div class="et-slider-arrows"><a class="et-arrow-prev" href="#">' + settings.previous_text + '</a><a class="et-arrow-next" href="#">' + settings.next_text + '</a></div>' );
+				else
+					$et_slider.append( settings.manual_arrows );
+
+				$et_slider_arrows 	= $( settings.arrows );
+				$et_slider_prev 	= $et_slider_arrows.find( settings.prev_arrow );
+				$et_slider_next 	= $et_slider_arrows.find( settings.next_arrow );
+
+				$et_slider_next.click( function(){
+					et_slider_move_to( 'next' );
+
+					return false;
+				} );
+
+				$et_slider_prev.click( function(){
+					et_slider_move_to( 'previous' );
+
+					return false;
+				} );
+			}
+
+			if ( settings.use_controls && et_slides_number > 1 ) {
+				for ( var i = 1; i <= et_slides_number; i++ ) {
+					controls_html += '<a href="#"' + ( i == 1 ? ' class="' + settings.control_active_class + '"' : '' ) + '>' + i + '</a>';
+				}
+
+				controls_html =
+					'<div class="' + settings.controls_class + '">' +
+						controls_html +
+					'</div>';
+
+				if ( settings.append_controls_to == '' )
+					$et_slider.append( controls_html );
+				else
+					$( settings.append_controls_to ).append( controls_html );
+
+				$et_slider_controls	= $et_slider.find( settings.controls ),
+
+				$et_slider_controls.click( function(){
+					et_slider_move_to( $(this).index() );
+
+					return false;
+				} );
+			}
+
+			et_slider_auto_rotate();
+
+			function et_slider_auto_rotate(){
+				if ( settings.slideshow && et_slides_number > 1 ) {
+					et_slider_timer = setTimeout( function() {
+						et_slider_move_to( 'next' );
+					}, settings.slideshow_speed );
+				}
+			}
+
+			function et_slider_move_to( direction ) {
+				var $active_slide = $et_slide.eq( et_active_slide ),
+					$next_slide;
+
+				if ( direction == 'next' || direction == 'previous' ){
+
+					if ( direction == 'next' )
+						et_active_slide = ( et_active_slide + 1 ) < et_slides_number ? et_active_slide + 1 : 0;
+					else
+						et_active_slide = ( et_active_slide - 1 ) >= 0 ? et_active_slide - 1 : et_slides_number - 1;
+
+				} else {
+
+					if ( et_active_slide == direction ) return;
+
+					et_active_slide = direction;
+
+				}
+
+				$next_slide	= $et_slide.eq( et_active_slide );
+
+				$et_slider_controls.removeClass( settings.control_active_class ).eq( et_active_slide ).addClass( settings.control_active_class );
+
+				$active_slide.animate( { opacity : 0 }, et_fade_speed, function(){
+					$(this).css('display', 'none');
+
+					$next_slide.css( { 'display' : 'block', opacity : 0 } ).animate( { opacity : 1 }, et_fade_speed );
+				} );
+
+				if ( typeof et_slider_timer != 'undefined' ) {
+					clearInterval( et_slider_timer );
+					et_slider_auto_rotate();
+				}
+			}
+		} );
+	}
+
+	$(document).ready( function(){
+		var $featured_slider 	= $( '.slider-wrapper' );
+
+		if ( $featured_slider.length ){
+			$featured_slider.append( '<div id="et-slider-controls"><div class="container"></div></div>' );
+
+			et_slider_settings = {
+				append_controls_to : '#et-slider-controls .container'
+			}
+
+			if ( $featured_slider.hasClass('et_slider_auto') ) {
+				var et_slider_autospeed_class_value = /et_slider_speed_(\d+)/g;
+
+				et_slider_settings.slideshow = true;
+
+				et_slider_autospeed = et_slider_autospeed_class_value.exec( $featured_slider.attr('class') );
+
+				et_slider_settings.slideshow_speed = et_slider_autospeed[1];
+			}
+
+			$featured_slider.et_simple_slider( et_slider_settings );
+		}
+	});
+})($)
